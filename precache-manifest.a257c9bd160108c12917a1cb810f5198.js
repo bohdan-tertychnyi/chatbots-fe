d5d44ba24a324b16ecc6b77a3da73f00994229a9
Bohdan Tertychnyi <bogdan.dev.w@gmail.com>
2020-09-06 22:40:48 +0300

    Updates

---
+++ precache-manifest.a257c9bd160108c12917a1cb810f5198.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f060974edec17e95fc065132aa1d32c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a50559d798eb84aaf297",
+    "url": "/static/css/main.fb7772e6.chunk.css"
+  },
+  {
+    "revision": "5e7815f4009dda8b9f80",
+    "url": "/static/js/2.af8aadc0.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/static/js/2.af8aadc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a50559d798eb84aaf297",
+    "url": "/static/js/main.648facaa.chunk.js"
+  },
+  {
+    "revision": "8a72ca634f9407012bb2",
+    "url": "/static/js/runtime-main.f6edf3a1.js"
+  }
+]);
